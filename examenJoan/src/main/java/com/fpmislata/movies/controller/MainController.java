936df5e7573976cb1aba37a239231f20549d7a4b
joan <joanaparici22@gmail.com>
2023-11-22 00:18:16 +0100

    Creado proyecto ExamenJoan para Servidor

---
+++ examenJoan/src/main/java/com/fpmislata/movies/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.fpmislata.movies.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("")
+    public String main() {
+        return "Peliculas Joan";
+    }
+
+}
