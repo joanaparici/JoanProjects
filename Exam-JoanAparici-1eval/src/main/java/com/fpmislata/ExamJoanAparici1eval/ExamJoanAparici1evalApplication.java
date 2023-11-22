f7ddaaa3a37abc7bc9b44b68ed1864b512fd22ec
joan <joanaparici22@gmail.com>
2023-11-22 09:11:34 +0100

    Creado plantilla examen para Servidor

---
+++ Exam-JoanAparici-1eval/src/main/java/com/fpmislata/ExamJoanAparici1eval/ExamJoanAparici1evalApplication.java
@@ -0,0 +1,13 @@
+package com.fpmislata.ExamJoanAparici1eval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamJoanAparici1evalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamJoanAparici1evalApplication.class, args);
+	}
+
+}
---
+++ Exam-JoanAparici-1eval/src/test/java/com/fpmislata/ExamJoanAparici1eval/ExamJoanAparici1evalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpmislata.ExamJoanAparici1eval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamJoanAparici1evalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
