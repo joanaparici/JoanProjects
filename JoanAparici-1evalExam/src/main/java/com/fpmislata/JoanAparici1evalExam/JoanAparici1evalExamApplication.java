382310db0adb239650ad5c3bcc19bbd85312f9d6
joan <joanaparici22@gmail.com>
2023-11-22 09:43:22 +0100

    Borradas plantillas con errores y creado proyecto servidor examen

--- JoanAparici-1evalExam/src/main/java/com/fpmislata/JoanAparici1evalExam/JoanAparici1evalExamApplication.java
+++ JoanAparici-1evalExam/src/main/java/com/fpmislata/JoanAparici1evalExam/JoanAparici1evalExamApplication.java
@@ -1,13 +1,13 @@
-package com.fpmislata.EvalExamJoanAparici;
+package com.fpmislata.JoanAparici1evalExam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class JoanAparici1evalExamApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(JoanAparici1evalExamApplication.class, args);
 	}
 
 }
--- JoanAparici-1evalExam/src/test/java/com/fpmislata/JoanAparici1evalExam/JoanAparici1evalExamApplicationTests.java
+++ JoanAparici-1evalExam/src/test/java/com/fpmislata/JoanAparici1evalExam/JoanAparici1evalExamApplicationTests.java
@@ -1,10 +1,10 @@
-package com.fpmislata.EvalExamJoanAparici;
+package com.fpmislata.JoanAparici1evalExam;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class JoanAparici1evalExamApplicationTests {
 
 	@Test
 	void contextLoads() {
